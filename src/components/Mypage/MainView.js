54614f4ff08603c7a08d6f9ca6c31abd7ea26e96
wonizz <growuplove7@naver.com>
2020-06-17 11:05:48 +0900

    commit

--- src/components/Mypage/MainView.js
+++ src/components/Mypage/MainView.js
@@ -16,55 +16,55 @@ const mapDispatchToProps = dispatch => ({
 });
 
 const MainView = props => {
-  if(!props.token) return;
-  if(!props.rsrvList){
-    return(
+  if (!props.token) return;
+  if (!props.rsrvList) {
+    return (
       <div className="cont-list"></div>
     )
   }
   const clickHandler = ev => {
     ev.preventDefault();
-    window.booktitle=ev.currentTarget.attributes[0].value;
-    window.email=ev.currentTarget.attributes[1].value;
-    window.returndate=window.getTimeStamp();
+    window.booktitle = ev.currentTarget.attributes[0].value;
+    window.email = ev.currentTarget.attributes[1].value;
+    window.returndate = window.getTimeStamp();
     //this.props.onLoadMore(agent.Articles.onLoadMore(20, window.number))
   };
   return (
-    <table className="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th>도서명</th>
-            <th>대출일</th>
-            <th>반납예정일</th>
-            <th>반납일</th>
-            <th>액션</th>
-          </tr>
-        </thead>
-        <tbody>
-            {
-              props.rsrvList.map((reservation, index) => {
-                return (
-                  <tr>
-                    <td>{reservation.booktitle}</td>
-                    <td>{reservation.rsrvdate}</td>
-                    <td>{reservation.duedate}</td>
-                    <td>{reservation.returndate}</td>
-                    <td>
-                        {
-                          `${reservation.returndate}`=== "undefined" 
-                          ? (<Link to="#returnModal" title={reservation.booktitle} email={props.email.id} onClick={clickHandler} data-toggle="modal">
-                              <button className="btn btn-primary">반납하기</button>
-                          </Link>)
-                          : ('')  
-                        }
-                    </td>
-                  </tr>
-                );
-              })
-            }
+    <table className="table table-striped table-bordered dataTable no-footer">
+      <thead>
+        <tr>
+          <th>도서명</th>
+          <th>대출일</th>
+          <th>반납예정일</th>
+          <th>반납일</th>
+          <th>반납신청</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          props.rsrvList.map((reservation, index) => {
+            return (
+              <tr>
+                <td>{reservation.booktitle}</td>
+                <td>{reservation.rsrvdate}</td>
+                <td>{reservation.duedate}</td>
+                <td>{reservation.returndate}</td>
+                <td>
+                  {
+                    `${reservation.returndate}` === "undefined"
+                      ? (<Link to="#returnModal" title={reservation.booktitle} email={props.email.id} onClick={clickHandler} data-toggle="modal">
+                        <button className="btn btn-primary">반납하기</button>
+                      </Link>)
+                      : ('')
+                  }
+                </td>
+              </tr>
+            );
+          })
+        }
 
-            </tbody>
-        </table>
+      </tbody>
+    </table>
   );
 };
 
